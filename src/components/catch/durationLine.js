f41a51e01dc89e18b83ee50a39d7750a83b2f452
JulienRobberechts <robberechts.consulting@gmail.com>
2020-03-10 21:41:20 +0100

    :lipstick: dot line

--- src/components/catch/durationLine.js
+++ src/components/catch/durationLine.js
@@ -13,29 +13,53 @@ const DurationLine = ({
   console.log("travelDurationPercentage", travelDurationPercentage);
   console.log("waitingDurationPercentage", waitingDurationPercentage);
   return (
-    <TargetTime>
-      <TravelTime percentage={travelDurationPercentage}>
-        <IconContainer>
-          <Walk />
-        </IconContainer>
-        <TimeSpan timeSpan={travelDuration} />
-        <IconContainer>
-          <Walk />
-        </IconContainer>
-      </TravelTime>
-      <WaitingTime percentage={waitingDurationPercentage}>
-        <TimeSpan timeSpan={waitingDuration} />
-      </WaitingTime>
-    </TargetTime>
+    <>
+      <TargetTime>
+        <TravelTime percentage={travelDurationPercentage}>
+          <IconContainer>
+            <Walk />
+          </IconContainer>
+          <TimeSpan timeSpan={travelDuration} />
+          <IconContainer>
+            <Walk />
+          </IconContainer>
+        </TravelTime>
+        <WaitingTime percentage={waitingDurationPercentage}>
+          <TimeSpan timeSpan={waitingDuration} />
+        </WaitingTime>
+      </TargetTime>
+      <TargetTime2>
+        <TravelTime2 percentage={travelDurationPercentage}>
+          <DottedLine />
+        </TravelTime2>
+        <WaitingTime percentage={waitingDurationPercentage}></WaitingTime>
+      </TargetTime2>
+    </>
   );
 };
-
+const DottedLine = styled.hr`
+  border: none;
+  border-top: 6px dotted black;
+  color: #fff;
+  height: 8px;
+  width: 98%;
+  margin: 0.1rem 0.5rem;
+`;
 const TargetTime = styled.div`
   background-color: ${() => colors.color3};
   display: flex;
   align-items: stretch;
   width: 100%;
   height: 65px;
+  radius
+`;
+
+const TargetTime2 = styled.div`
+  background-color: ${() => colors.color3};
+  display: flex;
+  align-items: stretch;
+  width: 100%;
+  height: 10px;
 `;
 
 const TravelTime = styled.div`
@@ -44,6 +68,11 @@ const TravelTime = styled.div`
   display: flex;
   justify-content: space-around;
   align-items: stretch;
+`;
+
+const TravelTime2 = styled.div`
+  background-color: ${() => colors.color5};
+  width: ${props => props.percentage + "%"};
 `;
 
 const WaitingTime = styled.div`
