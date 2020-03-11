94d799e36d66c81c32599cbb29818a1984816f0d
JulienRobberechts <robberechts.consulting@gmail.com>
2020-03-11 13:52:56 +0100

    :recycle: clean css

--- src/components/catch/timeSpan.js
+++ src/components/catch/timeSpan.js
@@ -3,9 +3,7 @@ import styled from "styled-components";
 import { colors } from "../../design/colors";
 
 const TimeSpan = ({ timeSpan, displayPositiveSign = false }) => {
-  // console.log("timeSpan", timeSpan);
   const totalMilliseconds = timeSpan.valueOf() * 1000;
-  // console.log("totalMilliseconds", totalMilliseconds);
   const negative = totalMilliseconds < 0;
   const positive = totalMilliseconds > 0;
 
@@ -14,44 +12,36 @@ const TimeSpan = ({ timeSpan, displayPositiveSign = false }) => {
 
   return (
     <Panel>
-      <Panel2>
-        {negative && <Sign>-</Sign>}
-        {displayPositiveSign && positive && <Sign>+</Sign>}
-        {minutes !== 0 && (
-          <>
-            <NumberText>{minutes}</NumberText>
-            <SymbolText>min</SymbolText>
-          </>
-        )}
-        <NumberText>
-          {seconds.toLocaleString("en-US", {
-            minimumIntegerDigits: 2,
-            useGrouping: false
-          })}
-        </NumberText>
-        <SymbolText>s</SymbolText>
-      </Panel2>
+      {negative && <Sign>-</Sign>}
+      {displayPositiveSign && positive && <Sign>+</Sign>}
+      {minutes !== 0 && (
+        <>
+          <NumberText>{minutes}</NumberText>
+          <SymbolText>min</SymbolText>
+        </>
+      )}
+      <NumberText>
+        {seconds.toLocaleString("en-US", {
+          minimumIntegerDigits: 2,
+          useGrouping: false
+        })}
+      </NumberText>
+      <SymbolText>s</SymbolText>
     </Panel>
   );
 };
 
-const Panel = styled.span`
+const Panel = styled.div`
+  margin: 0 auto;
   display: flex;
-  flex-direction: column;
   justify-content: center;
   align-items: stretch;
   color: ${() => colors.color6};
   overflow: hidden;
 `;
 
-const Panel2 = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: stretch;
-`;
-
 const NumberText = styled.span`
-  margin: 0 0.1rem;
+  margin: 0 0.05rem;
 `;
 
 const SymbolText = styled.span`
