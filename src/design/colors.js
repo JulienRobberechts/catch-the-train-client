93082a0369a9d8329e6b78b4a25a861e8fae5fc2
JulienRobberechts <robberechts.consulting@gmail.com>
2020-03-11 20:14:37 +0100

    :lipstick: new colors

--- src/design/colors.js
+++ src/design/colors.js
@@ -1,4 +1,4 @@
-const colors = {
+const palette1 = {
   color1: "#f3dcdb",
   color2: "#e4c1c3",
   color3: "#cfb5cb",
@@ -6,5 +6,16 @@ const colors = {
   color5: "#747073",
   color6: "#403e3f"
 };
+
+const palette2 = {
+  color1: "#f4f1de", // background
+  color2: "#f2cc8f", //
+  color3: "#81b29a", //
+  color4: "#e07a5f", // attention
+  color5: "#3d405b",
+  color6: "#403e3f" // dark strings
+};
+
+const colors = palette2;
 
 export { colors };
