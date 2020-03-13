ed0dc5a280b2d85b0d6a35f6545ea5fd3d489d52
JulienRobberechts <robberechts.consulting@gmail.com>
2020-03-13 18:10:18 +0100

    :white_check_mark: TimeSpan tests

--- src/components/time/timeSpan.test.js
+++ src/components/time/timeSpan.test.js
@@ -2,19 +2,34 @@ import React from "react";
 import { render } from "@testing-library/react";
 import moment from "moment";
 import TimeSpan from "./timeSpan";
+import {
+  withTextContent,
+  notExpectInTheDocument
+} from "../../tests/utils/testing-library-react";
 
 describe("timeSpan", () => {
-  const span2m30 = moment.duration({
+  const span2m33 = moment.duration({
     minutes: 2,
-    seconds: 30
+    seconds: 33
   });
-  test("should display 2 min 30", () => {
-    const { getByText, queryByText } = render(<TimeSpan timeSpan={span2m30} />);
-    expect(queryByText("0")).not.toBeInTheDocument();
-    expect(queryByText("00")).not.toBeInTheDocument();
-    expect(queryByText("h")).not.toBeInTheDocument();
-    getByText("2");
-    getByText("min");
-    getByText("30");
+
+  const span1h02m33 = moment.duration({
+    hours: 1,
+    minutes: 2,
+    seconds: 33
+  });
+
+  test("should display 2 min 33", () => {
+    const { getByText, queryByText } = render(<TimeSpan timeSpan={span2m33} />);
+    getByText(withTextContent("2min33s"));
+    notExpectInTheDocument(queryByText, "0", "00", "02", "h", "H");
+  });
+
+  test("should display 1h 2 min 33", () => {
+    const { getByText, queryByText } = render(
+      <TimeSpan timeSpan={span1h02m33} />
+    );
+    getByText(withTextContent("1h2min"));
+    notExpectInTheDocument(queryByText, "0", "00", "01", "3", "33", "m", "min");
   });
 });
