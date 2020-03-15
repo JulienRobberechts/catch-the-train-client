5308fe67957ae20c9cb69f3d674afe76de08b854
JulienRobberechts <robberechts.consulting@gmail.com>
2020-03-15 10:52:06 +0100

    :white_check_mark: tests getDelayStatus refactor

--- src/domains/toTheStation/pure/getDelayStatus.test.js
+++ src/domains/toTheStation/pure/getDelayStatus.test.js
@@ -2,57 +2,37 @@ import moment from "moment";
 import getDelayStatus from "./getDelayStatus";
 import each from "jest-each";
 
-describe("train helpers", () => {
+describe("toTheStation", () => {
   describe("getDelayStatus", () => {
-    const durationPlus2m30 = moment.duration({
-      minutes: 2,
-      seconds: 30
-    });
-
-    test("should identify an early delay", () => {
-      const onTimeMarginDelaySeconds = 20;
-      const delayStatus = getDelayStatus(
-        durationPlus2m30,
-        onTimeMarginDelaySeconds
-      );
-      expect(delayStatus).toBe("early");
-    });
-    test("should identify an late delay", () => {
-      const delayDuration = moment.duration({
-        minutes: -2,
-        seconds: 30
-      });
-      const onTimeMarginDelaySeconds = 20;
-      const delayStatus = getDelayStatus(
-        delayDuration,
-        onTimeMarginDelaySeconds
-      );
-      expect(delayStatus).toBe("late");
-    });
-    test("should identify an ontime delay of +15s", () => {
-      const delayDuration = moment.duration({
-        minutes: 0,
-        seconds: 15
-      });
-      const onTimeMarginDelaySeconds = 20;
-      const delayStatus = getDelayStatus(
-        delayDuration,
-        onTimeMarginDelaySeconds
-      );
-      expect(delayStatus).toBe("ontime");
-    });
-    test("should identify an ontime delay of -15s", () => {
-      const delayDuration = moment.duration({
-        minutes: 0,
-        seconds: -15
-      });
-      const onTimeMarginDelaySeconds = 20;
-      const delayStatus = getDelayStatus(
-        delayDuration,
-        onTimeMarginDelaySeconds
-      );
-      expect(delayStatus).toBe("ontime");
-    });
+    each([
+      ["-01:00:00", 20, "late"],
+      ["-00:02:00", 20, "late"],
+      ["-00:00:25", 20, "late"],
+      ["-00:00:20", 20, "ontime"],
+      ["-00:00:05", 20, "ontime"],
+      ["00:00:00", 20, "ontime"],
+      ["00:00:05", 20, "ontime"],
+      ["00:00:20", 20, "ontime"],
+      ["00:00:30", 20, "early"],
+      ["00:02:00", 20, "early"],
+      ["-00:03:00", 120, "late"],
+      ["-00:01:30", 120, "ontime"],
+      ["-00:00:15", 120, "ontime"],
+      ["00:00:00", 120, "ontime"],
+      ["00:00:20", 120, "ontime"],
+      ["00:02:00", 120, "ontime"],
+      ["00:03:00", 120, "early"]
+    ]).test(
+      "should consider '%s' with %s second margin as '%s'",
+      (delayDurationString, onTimeMarginDelaySeconds, expectedDelayStatus) => {
+        const delayDuration = moment.duration(delayDurationString);
+        const actualDelayStatus = getDelayStatus(
+          delayDuration,
+          onTimeMarginDelaySeconds
+        );
+        expect(actualDelayStatus).toBe(expectedDelayStatus);
+      }
+    );
     test("should identify an invalid delay", () => {
       const delayDuration = "invalid delay";
       const onTimeMarginDelaySeconds = 20;
