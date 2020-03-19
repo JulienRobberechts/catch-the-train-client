6fdd0b0a0fba22910c5fc5896c54be0ff52776d6
JulienRobberechts <robberechts.consulting@gmail.com>
2020-03-19 08:15:24 +0100

    :wrench: set server url into config

---
+++ src/config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  SERVER_ROOT_URL:
+    process.env.REACT_APP_SERVER_ROOT_URL || "http://localhost:3000"
+};
--- src/domains/timeTable/sagas.js
+++ src/domains/timeTable/sagas.js
@@ -1,19 +1,24 @@
 import { put, call, takeLatest } from "redux-saga/effects";
 import axios from "axios";
 import { fetch } from "./slice";
+import { SERVER_ROOT_URL } from "../../config";
 
 // const delay = ms => new Promise(res => setTimeout(res, ms));
 
 /** function that returns an axios call */
 export function getTimeTable() {
   // console.log("getTimeTable start *********************************");
-  return axios.request("http://localhost:3034/schedules");
+  const url = SERVER_ROOT_URL + "/schedules";
+  return axios.request(url);
 }
 
 export function* getTimeTableEffectSaga(action) {
   try {
     const result = yield call(getTimeTable);
     // console.log("RESULT----------------------------", result);
+    if (!result) {
+      throw Error("no data from the server");
+    }
     yield put({ type: "timeTable/fetchSuccess", payload: result.data });
     // console.log("getTimeTableEffectSaga 3");
   } catch (error) {
