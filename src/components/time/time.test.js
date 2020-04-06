a1297e1f28a183907b46fc80c8dc436050a09838
JulienRobberechts <robberechts.consulting@gmail.com>
2020-04-06 12:39:54 +0200

    :white_check_mark: clean up Time React component test

--- src/components/time/time.test.js
+++ src/components/time/time.test.js
@@ -2,7 +2,6 @@ import React from "react";
 import { render } from "@testing-library/react";
 import moment from "moment";
 import each from "jest-each";
-import jstz from "jstz";
 import Time from "./time";
 
 describe("Time React component", () => {
@@ -20,14 +19,6 @@ describe("Time React component", () => {
   `.test(
     "should display $time as $expectedTextContent",
     ({ time, displaySeconds, expectedTextContent }) => {
-      const timezone = jstz.determine();
-      const timeZoneName = `${timezone.name()}: ${timezone.stdTimezoneOffset()} mins usually and ${timezone.timezoneOffset()} mins now`;
-      console.log({ timeZoneName });
-
-      // expect(timezone.name()).toBe("Europe/Paris");
-
-      // moment.parseZone() parses the string but keeps the resulting Moment object
-      // in a fixed-offset timezone with the provided offset in the string.
       const timeMoment = moment.parseZone(time);
       const { container } = render(
         <Time time={timeMoment} displaySeconds={displaySeconds} />
