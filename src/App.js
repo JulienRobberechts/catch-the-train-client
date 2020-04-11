52ca39075c5d72610860dbdf7befc906d696c7fd
JulienRobberechts <robberechts.consulting@gmail.com>
2020-04-11 21:11:35 +0200

    :lipstick: sation selection ui v1

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "semantic-ui-css/semantic.min.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import styled from "styled-components";
 import { Provider } from "react-redux";
--- src/components/selectionPage/index.js
+++ src/components/selectionPage/index.js
@@ -2,26 +2,119 @@ import React from "react";
 import styled from "styled-components";
 import { Helmet } from "react-helmet";
 import { colors } from "../../design/colors";
+import { Formik, Form, useField } from "formik";
+import stations from "../../data/ratp/rers/A/stations.json";
+import { Button, Header, Dropdown } from "semantic-ui-react";
+
+const DropdownSemantic = ({ ...props }) => {
+  const [field, meta, helpers] = useField(props);
+  const finalProps = { ...props, ...field };
+
+  // Can be useful to act on the full formik
+  // const formikContext = useFormikContext();
+  // console.log("DropdownSemantic formikContext ", { formikContext });
+
+  // console.log("DropdownSemantic helpers ", { helpers });
+  // console.log("DropdownSemantic", { props });
+  // console.log("DropdownSemantic", { field });
+  // console.log("DropdownSemantic", { meta });
+  // console.log("DropdownSemantic", { finalProps });
+
+  return (
+    <Dropdown
+      {...finalProps}
+      onChange={(e, data) => {
+        helpers.setValue(data.value);
+        const touched = data.value !== meta.initialValue;
+        console.log({ touched });
+        helpers.setTouched(data.value !== meta.initialValue);
+      }}
+    />
+  );
+};
 
 const SelectionPage = () => {
+  const stationOptions = stations.map((station) => ({
+    key: station.slug,
+    value: station.slug,
+    text: station.name,
+  }));
+
   return (
     <>
       <Helmet>
         <title>Selection</title>
       </Helmet>
       <Title>Selection</Title>
-
-      <SectionTitle>Réseau</SectionTitle>
-      <Section>RER</Section>
-      <SectionTitle>Ligne</SectionTitle>
-      <Section>A</Section>
-      <SectionTitle>Gare</SectionTitle>
-      <Section>...</Section>
-      <SectionTitle>Destination</SectionTitle>
-      <Section>...</Section>
+      <Formik
+        initialValues={{ departure: "", destination: "" }}
+        onSubmit={(data, { setSubmitting }) => {
+          setTimeout(() => {
+            alert(JSON.stringify(data, null, 2));
+            setSubmitting(false);
+          }, 400);
+        }}
+      >
+        {(formik) => (
+          <Form>
+            <SectionTitle>Réseau</SectionTitle>
+            <Section>RER</Section>
+            <SectionTitle>Ligne</SectionTitle>
+            <Section>A</Section>
+            <SectionTitle>Gare de départ</SectionTitle>
+            <Section>
+              <FieldContainer>
+                <DropdownSemantic
+                  name="departure"
+                  type="select"
+                  placeholder="Gare de départ"
+                  fluid
+                  search
+                  selection
+                  clearable
+                  options={stationOptions}
+                />
+              </FieldContainer>
+            </Section>
+            <SectionTitle>Destination</SectionTitle>
+            <Section>
+              <FieldContainer>
+                <DropdownSemantic
+                  name="destination"
+                  type="select"
+                  placeholder="Gare de destination"
+                  fluid
+                  search
+                  selection
+                  clearable
+                  options={stationOptions}
+                />
+              </FieldContainer>
+            </Section>
+            <SubmitButtonContainer>
+              <Button type="submit">Voir les prochains départs</Button>
+            </SubmitButtonContainer>
+            <h1>Formik</h1>
+            <pre>{JSON.stringify(formik, null, 3)}</pre>
+          </Form>
+        )}
+      </Formik>
     </>
   );
 };
+
+const FieldContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  max-width: 500px;
+`;
+
+const SubmitButtonContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  max-width: 500px;
+  padding: 1rem;
+`;
 
 const Title = styled.div`
   color: ${() => colors.dark.text.normal};
@@ -29,8 +122,8 @@ const Title = styled.div`
   margin: 1rem 1rem;
 `;
 
-const SectionTitle = styled.div`
-  color: ${() => colors.dark.text.normal};
+const SectionTitle = styled(Header).attrs(() => ({ as: "h2" }))`
+  color: ${() => colors.dark.text.original};
   font-size: 1.1rem;
   margin: 0.5rem 1rem;
 `;
