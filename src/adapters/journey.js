c9f62e8b65656ebf14cf35ed73701a27d881ffa8
JulienRobberechts <robberechts.consulting@gmail.com>
2020-04-21 21:28:33 +0200

    :card_file_box: save full object in localstorage

--- src/adapters/journey.js
+++ src/adapters/journey.js
@@ -1,27 +1,34 @@
 // Local storage keys
-const lsNetwork = "network";
-const lsLine = "line";
-const lsDeparture = "departure";
-const lsDestination = "destination";
+// const lsNetwork = "network";
+// const lsLine = "line";
+// const lsDeparture = "departure";
+// const lsDestination = "destination";
+
+const LocalStorageKey_CurrentJourney = "journey";
 
 export function getJourney() {
   try {
-    return {
-      network: localStorage.getItem(lsNetwork),
-      line: localStorage.getItem(lsLine),
-      departure: localStorage.getItem(lsDeparture),
-      destination: localStorage.getItem(lsDestination),
-    };
+    const serializedItem = localStorage.getItem(LocalStorageKey_CurrentJourney);
+    if (serializedItem === null) {
+      return undefined;
+    }
+    return JSON.parse(serializedItem);
+    // return {
+    //   network: localStorage.getItem(lsNetwork),
+    //   line: localStorage.getItem(lsLine),
+    //   departure: localStorage.getItem(lsDeparture),
+    //   destination: localStorage.getItem(lsDestination),
+    // };
   } catch (error) {
     return undefined;
   }
 }
 
-export function setJourney({ network, line, departure, destination }) {
+export function setJourney(item) {
   try {
-    localStorage.setItem(lsNetwork, network);
-    localStorage.setItem(lsLine, line);
-    localStorage.setItem(lsDeparture, departure);
-    localStorage.setItem(lsDestination, destination);
-  } catch (error) {}
+    const serializedItem = JSON.stringify(item);
+    localStorage.setItem(LocalStorageKey_CurrentJourney, serializedItem);
+  } catch (error) {
+    // Ignore
+  }
 }
