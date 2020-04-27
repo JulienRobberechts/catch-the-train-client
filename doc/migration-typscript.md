40e2baa94be0b45ad02a846a2ae895317c2041ce
JulienRobberechts <robberechts.consulting@gmail.com>
2020-04-27 10:14:53 +0200

    :bulb: doc

---
+++ doc/migration-typscript.md
@@ -0,0 +1,7 @@
+# Migration to TypeScript
+
+## 1. Add types
+
+    ```cmd
+    npm i typescript @types/react @types/node @types/react-dom @types/jest
+    ```
--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,7 @@ import React from "react";
 import { render } from "@testing-library/react";
 import App from "./App";
 
-describe("App", () => {
+describe("App Page", () => {
   test("smoke test", () => {
     const { getAllByText } = render(<App />);
     getAllByText(/Attraper le train/i);
