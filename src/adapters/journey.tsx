494a353bf526813b8d157fc37f5f3409d1122f07
JulienRobberechts <robberechts.consulting@gmail.com>
2020-04-30 10:11:34 +0200

    :label: typescript migration file by file

--- src/adapters/journey.tsx
+++ src/adapters/journey.tsx
@@ -4,10 +4,17 @@ import {
 } from "./localStorage";
 const LocalStorageKey_CurrentJourney = "journey";
 
+export interface Journey {
+  network: string;
+  line: string;
+  departure: string;
+  destination: string;
+}
+
 export function loadJourney() {
   return loadDataFromLocalStorage(LocalStorageKey_CurrentJourney) ?? undefined;
 }
 
-export function saveJourney(item) {
+export function saveJourney(item: Journey) {
   saveDataIntoLocalStorage(LocalStorageKey_CurrentJourney, item);
 }
