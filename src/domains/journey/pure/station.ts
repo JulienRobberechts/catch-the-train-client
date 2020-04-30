3279e259e07889dcdce6f179a046c936384db884
JulienRobberechts <robberechts.consulting@gmail.com>
2020-04-30 07:39:21 +0200

    :label: typescript migration v3 - migration of 1 js to ts file

--- src/domains/journey/pure/station.ts
+++ src/domains/journey/pure/station.ts
@@ -1,4 +1,9 @@
-const getStation = (stationSlug, allStations) => {
+interface Station {
+  name: string;
+  slug: string;
+}
+
+const getStation = (stationSlug: string, allStations: Station[]) => {
   if (!allStations) return undefined;
   const station = allStations.find((s) => s.slug === stationSlug);
   return station;
