908f609b3ad7fa6de6df480910e82cb8730fd08b
JulienRobberechts <robberechts.consulting@gmail.com>
2020-06-02 21:18:24 +0200

    :construction: map icons

---
+++ src/components/selectDurationPage/assets/index.ts
@@ -0,0 +1,3 @@
+export { ReactComponent as MarkerUserIcon } from "./marker-user.svg";
+export { ReactComponent as MarkerStationIcon } from "./marker-station.svg";
+export { ReactComponent as GpsPositionIcon } from "./gps-position.svg";
