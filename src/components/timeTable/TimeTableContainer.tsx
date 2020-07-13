c96109b3fbba0624f9a029dbff68f28ebc21bbb0
JulienRobberechts <robberechts.consulting@gmail.com>
2020-07-13 09:45:02 +0200

    :recycle: step-2 main screen new design #1

--- src/components/timeTable/TimeTableContainer.tsx
+++ src/components/timeTable/TimeTableContainer.tsx
@@ -6,8 +6,6 @@ import TimeTable from "./timetable";
 import { selectCurrentTrainCode } from "../../domains/toTheStation/selectors";
 import { selectEnhancedTimeTable } from "../../domains/timeTableToTheStation/selectors";
 
-const NUMBER_OF_DEPARTURE_VISIBLE = 3;
-
 function TimeTableContainer() {
   const { push } = useHistory();
 
@@ -18,43 +16,13 @@ function TimeTableContainer() {
     return <div>...</div>;
   }
 
-  const currentIndex = Math.max(
-    0,
-    enhancedDepartures.findIndex(
-      (departure) => departure.trainCode === currentTrainCode
-    )
-  );
-
-  const minIndex = Math.max(
-    0,
-    Math.min(
-      currentIndex - 1,
-      enhancedDepartures.length - NUMBER_OF_DEPARTURE_VISIBLE
-    )
-  );
-
-  const previousVisible = minIndex > 0;
-  const previousDeparture = enhancedDepartures[currentIndex - 1];
-  const previousLink = `/next-train/${previousDeparture?.trainCode}`;
-
-  const nextVisible =
-    minIndex < enhancedDepartures.length - NUMBER_OF_DEPARTURE_VISIBLE;
-  const nextDeparture = enhancedDepartures[currentIndex + 1];
-  const nextLink = `/next-train/${nextDeparture?.trainCode}`;
-
   return (
     <TimeTable
       enhancedDepartures={enhancedDepartures}
-      minIndex={minIndex}
       currentTrainCode={currentTrainCode}
       handleDepartureNav={(targetTrainCode) => () => {
         push(`/next-train/${targetTrainCode}`);
       }}
-      previousVisible={previousVisible}
-      handlePreviousDepartureNav={() => push(previousLink)}
-      nextVisible={nextVisible}
-      handleNextDepartureNav={() => push(nextLink)}
-      numberOfDepartureVisible={NUMBER_OF_DEPARTURE_VISIBLE}
     />
   );
 }
--- src/components/timeTable/timetable.tsx
+++ src/components/timeTable/timetable.tsx
@@ -2,31 +2,18 @@ import React from "react";
 import styled from "styled-components";
 import { colors } from "../../design/colors";
 import Departure from "./departure";
-import { ArrowLeft, ArrowRight, More, Clock } from "../../design/icons";
 import { EnhancedDeparture } from "../../domains/timeTableToTheStation/types";
 
 interface Props {
-  enhancedDepartures?:  EnhancedDeparture[];
-  minIndex: number;
+  enhancedDepartures?: EnhancedDeparture[];
   currentTrainCode: string;
   handleDepartureNav: (targetTrainCode: string) => () => void;
-  previousVisible: boolean;
-  handlePreviousDepartureNav: ()=> void;
-  nextVisible: boolean;
-  handleNextDepartureNav:  ()=> void;
-  numberOfDepartureVisible: number;
 }
 
 function TimeTable({
   enhancedDepartures,
-  minIndex,
   currentTrainCode,
   handleDepartureNav,
-  previousVisible,
-  handlePreviousDepartureNav,
-  nextVisible,
-  handleNextDepartureNav,
-  numberOfDepartureVisible,
 }: Props) {
   if (!currentTrainCode) {
     return <PanelEmpty></PanelEmpty>;
@@ -35,22 +22,8 @@ function TimeTable({
   return (
     <Panel>
       <ScrollPanel>
-        {previousVisible ? (
-          <PreviousButton onClick={handlePreviousDepartureNav}>
-            <IconContainer>
-              <ArrowLeft />
-            </IconContainer>
-          </PreviousButton>
-        ) : (
-          <PlaceholderItem>
-            <IconContainer>
-              <Clock />
-            </IconContainer>
-          </PlaceholderItem>
-        )}
-        {enhancedDepartures && enhancedDepartures
-          .slice(minIndex, minIndex + numberOfDepartureVisible)
-          .map((departure) => (
+        {enhancedDepartures &&
+          enhancedDepartures.map((departure) => (
             <Departure
               key={departure.trainCode}
               selected={departure.trainCode === currentTrainCode}
@@ -58,19 +31,6 @@ function TimeTable({
               {...departure}
             />
           ))}
-        {nextVisible ? (
-          <NextButton onClick={handleNextDepartureNav}>
-            <IconContainer>
-              <ArrowRight />
-            </IconContainer>
-          </NextButton>
-        ) : (
-          <PlaceholderItem>
-            <IconContainer>
-              <More />
-            </IconContainer>
-          </PlaceholderItem>
-        )}
       </ScrollPanel>
     </Panel>
   );
@@ -81,7 +41,6 @@ const Panel = styled.div`
   color: ${() => colors.dark.panel.one.text.normal};
   margin-top: 0.3rem;
   border-radius: 3px;
-  height: 6rem;
   display: flex;
   flex-direction: column;
 `;
@@ -95,52 +54,9 @@ const ScrollPanel = styled.div`
   margin: 0.1rem;
   font-weight: bold;
   display: flex;
+  flex-direction: column;
   justify-content: space-between;
   align-items: stretch;
-`;
-
-const ScrollPanelItem = styled.button`
-  background: ${() => colors.dark.panel.one.background};
-  margin: 0.1rem;
-  padding: 0.4rem;
-  border-radius: 3px;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  cursor: pointer;
-
-  border: 1px solid;
-  flex-basis: 20%;
-`;
-
-const PreviousButton = styled(ScrollPanelItem)``;
-
-const NextButton = styled(ScrollPanelItem)``;
-
-const PlaceholderItem = styled(ScrollPanelItem)`
-  background: ${() => colors.dark.panel.one.background};
-  color: ${() => colors.dark.panel.one.text.normal};
-  margin: 0.3rem;
-  border-radius: 3px;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  border: none;
-  flex-basis: 20%;
-  text-align: center;
-
-  font-weight: normal;
-  font-size: 0.6rem;
-`;
-
-const IconContainer = styled.div`
-  svg {
-    width: 1.5rem;
-    height: 100%;
-    fill: ${() => colors.dark.panel.one.button.one.text.normal};
-  }
 `;
 
 export { TimeTable as default };
